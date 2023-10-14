8af9aa945825c21a43573fdde60542a535f78601
Keisuke-Igarashi <igarashi4948@gmail.com>
2023-10-15 08:43:19 +0900

    webappディレクトリを削除

--- webapp/sql/setup/0_setup.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `isucon`;
-CREATE USER IF NOT EXISTS `isucon`@`%` IDENTIFIED WITH mysql_native_password BY 'isucon';
-GRANT ALL ON `isucon`.* TO `isucon`@`%`;
-GRANT FILE ON *.* to 'isucon'@'%';
